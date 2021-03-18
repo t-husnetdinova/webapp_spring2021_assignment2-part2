aa12b38676b2a745bedb1145ea383f1d3be06677
Alex Verkest <31321596+iAmDragyn@users.noreply.github.com>
2021-03-17 20:28:11 -0600

    Some Vue stuff

---
+++ js/feed.js
@@ -0,0 +1,28 @@
+app.component('feed', {
+    data() {
+        return {
+            posts: 10,
+        }
+    },
+    template: 
+      /*html*/ 
+      `<div class="feed">
+        <post v-for="n in posts" :key="n">  </post>
+      </div>`
+  })
+
+app.component('post', {
+    template:
+    /* html */
+    `<div class="post">
+        <div class="card mx-auto homepage text-left">
+        <div class="card-body">
+            <h5 class="card-title"> Post </h5>
+            <p class="card-text"> Post Body </p>
+        </div>
+        <div class="card-footer text-muted text-left">
+            2 days ago
+        </div>
+        </div>
+      </div>`
+})
--- js/script.js
+++ js/script.js
@@ -17,3 +17,12 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+const app = Vue.createApp({
+  data() {
+    return {
+      componentKey: 0
+    }
+  },
+  methods: {}
+})
